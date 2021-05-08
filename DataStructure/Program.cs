7b82257285c939c4b5e99b0dd172a7db744d91f9
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-09 00:50:17 +0530

    initial commit

---
+++ DataStructure/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+    }
+    public class Node
+    {
+        public int data; //i have define two elements overthere
+        public Node next;
+
+        //constructor
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------Welcome To Data Structure Program-------");
+
+            LinkedList list = new LinkedList();//creating object of linkedlist class
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            Console.Read();
+        }
+    }
+}
