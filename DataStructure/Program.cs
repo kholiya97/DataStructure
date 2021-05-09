cf00953d3035dd6c70cba9f9d677f1449edfc20a
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-09 14:04:51 +0530

    Added file of UC2_LINKED_LIST_BY_ADDING

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -9,9 +9,9 @@ namespace DataStructure
     class LinkedList
     {
         public Node head;
-        public void InsertLast(int data)//creat method InserTlast 
+        public void InsertLast(int new_data)//creat method InserTlast 
         {
-            Node node = new Node(data);//
+            Node node = new Node(new_data);//
             if (this.head == null)
             {
                 this.head = node;
@@ -33,13 +33,7 @@ namespace DataStructure
             }
             return temp;
         }
-        public void InsertFront(int new_data)
-        {
-            Node new_node = new Node(new_data);
-            new_node.next = this.head;
-            this.head = new_node;
-            Console.WriteLine("inserted into front" + new_node);
-        }
+
         internal void Display()
         {
             Node temp = this.head;
@@ -57,34 +51,6 @@ namespace DataStructure
                 }
                 Console.WriteLine(temp.data);
             }
-        }
-        internal Node InsertAtParticularPosition(int position, int data)
-        {
-            if (position < 1)
-                Console.WriteLine("Invalid position");
-            if (position == 1)
-            {
-                var newNode = new Node(data);
-                newNode.next = this.head;
-                head = newNode;
-            }
-            else
-            {
-                while (position-- != 0)
-                {
-                    if (position == 1)
-                    {
-                        Node node = new Node(data);
-                        node.next = this.head.next;
-                        head.next = node;
-                        break;
-                    }
-                    head = head.next;
-                }
-                if (position != 1)
-                    Console.WriteLine("Position out of range");
-            }
-            return head;
         }
     }
     public class Node
@@ -106,14 +72,15 @@ namespace DataStructure
             Console.WriteLine("-------Welcome To Data Structure Program-------");
 
             LinkedList list = new LinkedList();//creating object of linkedlist class
-            list.InsertFront(70);
-            list.InsertFront(30);
-            list.InsertFront(56);
-
-
+            list.InsertLast(56);
+            list.InsertLast(30);
+            list.InsertLast(70);
             list.Display();
 
             Console.Read();
         }
     }
+
 }
+
+
