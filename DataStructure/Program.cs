d8fdad21aa1bda4d9cdcd4abc84e718e49b0ab42
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-09 13:50:03 +0530

    Added file of UC3_LINKED_LIST_BY_APPENDING

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -9,23 +9,83 @@ namespace DataStructure
     class LinkedList
     {
         public Node head;
-        public void Add(int data)
+        public void InsertLast(int data)//creat method InserTlast 
         {
-            Node node = new Node(data);
+            Node node = new Node(data);//
             if (this.head == null)
+            {
                 this.head = node;
+            }
             else
             {
-                Node temp = head;
+                Node lastNode = GetLastNode();
+                lastNode.next = node;
+            }
+            Console.WriteLine("inserted into list" + node.data);
+
+        }
+        public Node GetLastNode()
+        {
+            Node temp = this.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+        public void InsertFront(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.next = this.head;
+            this.head = new_node;
+            Console.WriteLine("inserted into front" + new_node);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            else
+            {
                 while (temp.next != null)
                 {
+                    Console.Write(temp.data + "->");
                     temp = temp.next;
                 }
-                temp.next = node;
+                Console.WriteLine(temp.data);
             }
-            Console.WriteLine("{0} inserted into the linked list", node.data);
         }
-
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
     }
     public class Node
     {
@@ -46,9 +106,12 @@ namespace DataStructure
             Console.WriteLine("-------Welcome To Data Structure Program-------");
 
             LinkedList list = new LinkedList();//creating object of linkedlist class
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
+            list.InsertFront(70);
+            list.InsertFront(30);
+            list.InsertFront(56);
+
+
+            list.Display();
 
             Console.Read();
         }
